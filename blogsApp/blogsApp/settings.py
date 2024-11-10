862e28cbf85760070f3b49b9b17e686a77f181a0
Raed Nimer <raed.nimer7@gmail.com>
2024-11-10 18:27:23 +0000

    Updates to settings.py

--- blogsApp/blogsApp/settings.py
+++ blogsApp/blogsApp/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'blog.apps.BlogConfig'
 ]
 
 MIDDLEWARE = [
@@ -56,7 +57,9 @@ ROOT_URLCONF = 'blogsApp.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [ 
+            BASE_DIR / 'templates'
+          ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
