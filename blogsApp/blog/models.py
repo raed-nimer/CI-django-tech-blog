4f237339fcfe4449c98779aea6f7ddb263380e5a
Raed Nimer <raed.nimer7@gmail.com>
2024-11-11 18:24:42 +0000

    Add blog model

--- blogsApp/blog/models.py
+++ blogsApp/blog/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+class Blog(models.Model):
+    title = models.CharField(max_length=100) # string - required
+    description = models.TextField(null=True, blank=True) # description is optional
+    image = models.ImageField(upload_to='images/', null=True, blank=True)
+    # category
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True) # Update 'updated_at' everytime the data updates.
+
+    def __str__(self):
+        return str(self.title)
