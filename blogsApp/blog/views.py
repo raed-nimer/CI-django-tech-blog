bb4f2567da61bbb7e21d160d8c365325204a7091
Raed Nimer <raed.nimer7@gmail.com>
2024-11-11 18:25:00 +0000

    Add views for the app

--- blogsApp/blog/views.py
+++ blogsApp/blog/views.py
@@ -1,3 +1,29 @@
 from django.shortcuts import render
-
+from .models import Blog
 # Create your views here.
+# Contains all our functions
+def about(request):
+    # calculations
+    return render(request, 'blog/about.html')
+    #return HttpResponse('This is About Page')
+
+# Function Responsible for managing and rendering home page.
+def home(request):
+    # Fetching from database
+    # DB code
+    blogs = Blog.objects.all() # fetching everything from blogs table
+    context = {
+        'blogs': blogs
+    }
+    return render(request, 'blog/home.html', context)
+    #return HttpResponse('Home Page')
+
+def contact(request):
+    return render(request, 'blog/contact.html')
+
+def blogDetails(request, pk):
+    blog = Blog.objects.get(id=pk)
+    context = {
+        'blog': blog
+    }
+    return render(request, 'blog/blogDetails.html', context)
