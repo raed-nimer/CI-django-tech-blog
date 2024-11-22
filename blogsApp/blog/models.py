aa91f731bcfb8860326f60093dda0be839ddc81b
Raed Nimer <raed.nimer7@gmail.com>
2024-11-22 17:02:20 +0000

    Add Category model

--- blogsApp/blog/models.py
+++ blogsApp/blog/models.py
@@ -2,6 +2,14 @@ from django.db import models
 
 # Create your models here.
 
+class Category(models.Model):
+    name = models.CharField(max_length=200) # required
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    
+    def __str__(self):
+        return str(self.name)
+
 class Blog(models.Model):
     title = models.CharField(max_length=100) # string - required
     description = models.TextField(null=True, blank=True) # description is optional
