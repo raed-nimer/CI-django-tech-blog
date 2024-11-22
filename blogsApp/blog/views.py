aa5710b522276b29297d2c7caad05ed25eb2a858
Raed Nimer <raed.nimer7@gmail.com>
2024-11-22 17:01:20 +0000

    Create add_blog view

--- blogsApp/blog/views.py
+++ blogsApp/blog/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
+#backend
+from django.shortcuts import render, redirect
 from .models import Blog
+from .forms import BlogForm
 # Create your views here.
 # Contains all our functions
 def about(request):
@@ -20,6 +22,25 @@ def home(request):
 
 def contact(request):
     return render(request, 'blog/contact.html')
+
+# Function responsible for showing the form + adding the blog.
+def add_blog(request):
+    if request.method == 'POST':
+        # request.POST => 
+        form = BlogForm(request.POST, request.FILES)
+        # Checking if the data is valid or not.
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+        else:
+            print("WE have an error")
+    
+    else:
+        form = BlogForm() # CREATING A FORM
+        context = {
+            'form': form
+        }
+        return render(request, 'blog/addBlog.html', context)
 
 def blogDetails(request, pk):
     blog = Blog.objects.get(id=pk)
