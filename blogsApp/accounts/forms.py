bc68d78669f5f73892ef39dc0ffd882f9ef09bc7
Raed Nimer <raed.nimer7@gmail.com>
2024-11-26 17:12:37 +0000

    Create RegisterUserForm

---
+++ blogsApp/accounts/forms.py
@@ -0,0 +1,17 @@
+import django.forms as forms
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
+
+class RegisterUserForm(UserCreationForm):
+    email: forms.EmailField()
+    
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'password1', 'password2']
+        widgets = {
+            'username': forms.TextInput(attrs={'class': 'form-control'}),
+            'email': forms.EmailInput(attrs={'class': 'form-control'}),
+            'password1': forms.PasswordInput(attrs={'class': 'form-control'}),
+            'password2': forms.PasswordInput(attrs={'class': 'form-control'})
+        }
+        
