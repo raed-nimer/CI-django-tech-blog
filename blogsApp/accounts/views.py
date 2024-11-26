8b6707bf2e67546456cae4a531e3f75044a0987a
Raed Nimer <raed.nimer7@gmail.com>
2024-11-26 17:02:36 +0000

    Create registerView

--- blogsApp/accounts/views.py
+++ blogsApp/accounts/views.py
@@ -1,2 +1,17 @@
 from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from .forms import RegisterUserForm
 # Create your views here.
+def registerView(request):
+    form = RegisterUserForm()
+    if request.method == 'POST':
+        form = RegisterUserForm(request.POST)
+        if form.is_valid():
+            form.save()
+            # Take the user to home page
+            return redirect('home')
+    
+    context = {
+        'form': form
+    }
+    return render(request, 'accounts/register.html', context)
