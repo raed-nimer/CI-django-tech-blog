63c82b3ac6c26bf52f5ab381b12dd6c5c4bf6d22
Raed Nimer <raed.nimer7@gmail.com>
2024-11-26 17:13:26 +0000

    Create register, login and logout routes

---
+++ blogsApp/accounts/urls.py
@@ -0,0 +1,15 @@
+from django.urls import path
+from django.http import HttpResponse
+from . import views
+from django.conf import settings
+from django.conf.urls.static import static
+
+urlpatterns = [
+    path('register/', views.registerView, name="register"), # register route
+    path('login/', views.loginView, name="login"), # login route
+    path('logout/', views.logoutView, name="logout") # logout route
+    
+]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
