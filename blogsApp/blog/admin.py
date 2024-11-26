c3d1437784250265269b006ef86bb3a7f5461a7e
Raed Nimer <raed.nimer7@gmail.com>
2024-11-26 17:04:41 +0000

    Add ContactFormResponse to admin.py

--- blogsApp/blog/admin.py
+++ blogsApp/blog/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Blog, Category
+from .models import Blog, Category, ContactFormResponse
 # Register your models here.
 admin.site.register(Blog)
 admin.site.register(Category)
+admin.site.register(ContactFormResponse)
