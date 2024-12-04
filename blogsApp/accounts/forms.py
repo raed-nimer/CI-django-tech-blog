4bee79b0b9c74b14a1c4b55c42ec1c420a7c8c82
Raed Nimer <raed.nimer7@gmail.com>
2024-12-04 18:04:44 +0000

    Create LoginForm

--- blogsApp/accounts/forms.py
+++ blogsApp/accounts/forms.py
@@ -15,3 +15,6 @@ class RegisterUserForm(UserCreationForm):
             'password2': forms.PasswordInput(attrs={'class': 'form-control'})
         }
         
+class LoginForm(forms.Form):
+    username = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control'}))
+    password = forms.CharField(widget=forms.PasswordInput(attrs={'class': 'form-control'}))
