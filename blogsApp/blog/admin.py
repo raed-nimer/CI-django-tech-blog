a40e2f4e8fc7ec4cd000366316d61f565c7350b1
Raed Nimer <raed.nimer7@gmail.com>
2024-12-16 18:19:23 +0000

    Add Comment model to admin.py

--- blogsApp/blog/admin.py
+++ blogsApp/blog/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Blog, Category, ContactFormResponse
+from .models import Blog, Category, ContactFormResponse, Comment
 # Register your models here.
 admin.site.register(Blog)
 admin.site.register(Category)
 admin.site.register(ContactFormResponse)
+admin.site.register(Comment)
