31f169778f4ea61e156bbfbc67ea4d63bcbb0f93
Raed Nimer <raed.nimer7@gmail.com>
2024-12-28 11:35:40 +0000

    Add first_name and last_name to register form

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,14 +1,18 @@
 import django.forms as forms
 from django.contrib.auth.models import User
-from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm, UserChangeForm
 
 class RegisterUserForm(UserCreationForm):
     email: forms.EmailField()
+    first_name: forms.CharField(max_length=100)
+    last_name: forms.CharField(max_length=100)
     
     class Meta:
         model = User
-        fields = ['username', 'email', 'password1', 'password2']
+        fields = ['username', 'first_name', 'last_name', 'email', 'password1', 'password2']
         widgets = {
+            'first_name': forms.TextInput(attrs={'class': 'form-control'}),
+            'last_name': forms.TextInput(attrs={'class': 'form-control'}),
             'username': forms.TextInput(attrs={'class': 'form-control'}),
             'email': forms.EmailInput(attrs={'class': 'form-control'}),
             'password1': forms.PasswordInput(attrs={'class': 'form-control'}),
