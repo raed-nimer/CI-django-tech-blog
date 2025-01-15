669ab7069ad6587dfd51b302656f8ee6ab0a8eb7
Raed Nimer <raed.nimer7@gmail.com>
2025-01-15 19:49:40 +0000

    Add urlpatterns and fix formatting

--- accounts/urls.py
+++ accounts/urls.py
@@ -13,6 +13,5 @@ urlpatterns = [
 
 ]
 
-if settings.DEBUG:
-    urlpatterns += static(
+urlpatterns += static(
         settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- blog/urls.py
+++ blog/urls.py
@@ -16,4 +16,5 @@ urlpatterns = [
     path('search/', views.searchBlogs, name="search"),  # search blogs
 ]
 
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(
+        settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
