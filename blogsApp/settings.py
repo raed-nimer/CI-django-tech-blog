253b223c8b65509222012dc5e527372af7357344
Raed Nimer <raed.nimer7@gmail.com>
2025-01-16 18:32:40 +0000

    Remove X_FRAME_OPTIONS

--- blogsApp/settings.py
+++ blogsApp/settings.py
@@ -19,8 +19,6 @@ if os.path.isfile('env.py'):
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-X_FRAME_OPTIONS = 'ALLOWALL'
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
