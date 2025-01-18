0690d4c675606120499812e52a5001839b6a36b2
Raed Nimer <raed.nimer7@gmail.com>
2025-01-18 11:24:23 +0000

    Move SECRET_KEY to env.py

--- blogsApp/settings.py
+++ blogsApp/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-@f-_&)+mxtaleml7d8z-zfmcg_&&*&)a-0^o0391bu)!r!fq^0'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production
 DEBUG = False
@@ -90,13 +90,6 @@ WSGI_APPLICATION = 'blogsApp.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
-
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
 
 if 'DATABASE_URL' not in os.environ:
     raise RuntimeError("The DATABASE_URL environment variable is required but not set.")
